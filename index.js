8c912bb8169a8b451fe61adb5c7f81c44b124b32
Bregy Malpartida <bregy.malpartida@utec.edu.pe>
2018-06-19 13:29:17 -0500

    asdasd

--- index.js
+++ index.js
@@ -1,22 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
-import { AppContainer } from 'react-hot-loader';
 
 import Presentation from './presentation';
 
-// const CustomErrorReporter = ({ error }) => <Redbox error={error} />;
-
-CustomErrorReporter.propTypes = {
-	error: PropTypes.instanceOf(Error).isRequired
-};
-
-ReactDOM.render(
-	<AppContainer errorReporter={CustomErrorReporter}>
-		<Presentation />
-	</AppContainer>,
-	document.getElementById('root')
-);
+ReactDOM.render(<Presentation />, document.getElementById('root'));
 
 // if (module.hot) {
 // 	module.hot.accept('./presentation', () => {
